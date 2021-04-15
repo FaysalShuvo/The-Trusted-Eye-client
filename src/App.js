09f40c1dd179be793a2f7801f44377e42b58be5f
Faysal Shuvo <32492440+FaysalShuvo@users.noreply.github.com>
2021-04-15 14:03:17 +0600

    Blank Slate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Alert } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about"></Route>
+          <Route path="/users"></Route>
+          <Route path="/">
+            <Alert variant="success">
+              <Alert.Heading>Hey, nice to see you</Alert.Heading>
+              <p>
+                Aww yeah, you successfully read this important alert message.
+                This example text is going to run a bit longer so that you can
+                see how spacing within an alert works with this kind of content.
+              </p>
+              <hr />
+              <p className="mb-0">
+                Whenever you need to, be sure to use margin utilities to keep
+                things nice and tidy.
+              </p>
+            </Alert>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
